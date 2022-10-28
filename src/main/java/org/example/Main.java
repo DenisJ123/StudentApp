ec3bed489edda2143553ee1c8f93810d4ed8c669
Denis Johnson <G00403126@atu.ie>
2022-10-28 23:38:10 +0100

    Final

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,51 @@
 package org.example;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Please enter Student Name");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("You entered: "+ name);
+
+        System.out.println("Please enter email");
+        Scanner input2 = new Scanner(System.in);
+        String email = input2.nextLine();
+        System.out.println("You entered: "+ email);
+
+        System.out.println("Please enter course");
+        Scanner input3 = new Scanner(System.in);
+        String course = input3.nextLine();
+        System.out.println("You entered: "+ course);
+
+        System.out.println("Please enter Student Name");
+        Scanner input4 = new Scanner(System.in);
+        String name2 = input4.nextLine();
+        System.out.println("You entered: "+ name2);
+
+        System.out.println("Please enter email");
+        Scanner input5 = new Scanner(System.in);
+        String email2 = input5.nextLine();
+        System.out.println("You entered: "+ email2);
+
+        System.out.println("Please enter course");
+        Scanner input6 = new Scanner(System.in);
+        String course2 = input6.nextLine();
+        System.out.println("You entered: "+ course2);
+
+        System.out.println("Please enter Student Name");
+        Scanner input7 = new Scanner(System.in);
+        String name3 = input.nextLine();
+        System.out.println("You entered: "+ name3);
+
+        System.out.println("Please enter email");
+        Scanner input8 = new Scanner(System.in);
+        String email3 = input2.nextLine();
+        System.out.println("You entered: "+ email3);
+
+        System.out.println("Please enter course");
+        Scanner input9 = new Scanner(System.in);
+        String course3 = input3.nextLine();
+        System.out.println("You entered: "+ course3);
     }
 }
